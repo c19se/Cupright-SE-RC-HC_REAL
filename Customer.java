d9d69f9e0549841de0b5003bb9ce555c94f99a50
c19se <c19se@dalton.org>
2017-04-26 09:45:44 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,4 +1,10 @@
 public class Customer{
+ Random gen = new Random();
  //customer money (randomly generate per customer) 
-  int money;
+ public Customer{
+  Drink drink = new Drink(Drink.type[gen.nextInt(Drink.type.length)], gen.nextInt(2));
+  
+ }
+ 
+  
 }  
