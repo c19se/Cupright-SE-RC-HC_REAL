aff29e5a6629e82d30ce866af41cf297562cc155
c19se <c19se@dalton.org>
2017-05-03 09:23:51 -0400

    Update CuprightCoffee.java

--- CuprightCoffee.java
+++ CuprightCoffee.java
@@ -1,1 +1,14 @@
+import java.util.concurrent.TimeUnit;
+
+
+public class CuprightCoffee{
+ 	public static void main(String[] args) throws InterruptedException {
+        Customer[] customers = new Customers[gen.nextInt(20) + 20];
+        for(Customer c: customers){
+          c = new Customer()
+        }
+    		TimeUnit.SECONDS.sleep(1);
+
+  }
+}
 
