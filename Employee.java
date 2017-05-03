8b96a3082d6113cef1b02402d7d1ca26c8029f35
c19se <c19se@dalton.org>
2017-05-03 09:13:36 -0400

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -2,6 +2,18 @@ public class Employee{
   int salary;
   String name;
   
+  double makeCoffee(Customer c){
+		c.hasDrink = true;
+		double cost = 0;
+		for(int i = 0; i<Drink.types.length; i++){
+			if(c.drink.type.equals(Drink.types[i])){
+				cost = Drink.prices[i][c.drink.size];
+//				System.out.println(cost);
+				break;
+			}
+		}
+		return cost;
+	}
   
   
   
