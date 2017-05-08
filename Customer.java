b7ca3a8f67e45a85667b1fee73d031a48b820e36
c19se <c19se@dalton.org>
2017-05-08 11:04:15 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,10 +1,18 @@
-public class Customer{
- Random gen = new Random();
- //customer money (randomly generate per customer) 
- public Customer{
-  Drink drink = new Drink(Drink.type[gen.nextInt(Drink.type.length)], gen.nextInt(2));
-  
- }
- 
-  
-}  
+
+public class Drink {
+	static String[] types = {"The Classic Cup", "The Hauté Cup", "The Hipster Cup", "House Blend", "Classic Earl Grey", "Traditional Oolong"};
+	static double[][] prices = {{3,4,5},{3,4,5},{3.5,4.5,5.5},{2,3,4},{2,3,4},{3,4,5}};
+	String type;
+	int size;
+	double cost;
+	public Drink(){
+
+	}
+	public Drink(int type_, int size_){
+		type = types[type_];
+		size = size_;
+		cost = prices[type_][size_];
+	}
+}
+
+
