109faa2f21a45399a9c464e378207240b7525656
c19se <c19se@dalton.org>
2017-05-08 11:04:54 -0400

    Update Drink.java

--- Drink.java
+++ Drink.java
@@ -4,12 +4,15 @@ public class Drink {
 	static double[][] prices = {{3,4,5},{3,4,5},{3.5,4.5,5.5},{2,3,4},{2,3,4},{3,4,5}};
 	String type;
 	int size;
-	
+	double cost;
 	public Drink(){
 
 	}
-	public Drink(String type_, int size_){
-		type = type_;
+	public Drink(int type_, int size_){
+		type = types[type_];
 		size = size_;
+		cost = prices[type_][size_];
 	}
 }
+
+
