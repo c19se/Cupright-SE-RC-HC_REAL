29d4b829a005d3571c36324e3537c850331692ec
c19hc <hhcarroll2001@gmail.com>
2017-05-09 22:19:44 -0400

    Update Drink.java

--- Drink.java
+++ Drink.java
@@ -2,7 +2,7 @@
 public class Drink {
 	static String[] types = {"The Classic Cup", "The Hauté Cup", "The Hipster Cup", "House Blend", "Classic Earl Grey", "Traditional Oolong"};
 	static double[][] prices = {{3,4,5},{3,4,5},{3.5,4.5,5.5},{2,3,4},{2,3,4},{3,4,5}};
-	static double[][] costs = {{.75,1.00,1.25 },{},{},{},{},{}};
+	static double[][] costs = {{.75,1.00,1.25 },{.75,1.00,1.25},{.85,1.09,1.35},{0.21,0.30,0.41},{0.21,0.30,0.41},{0.74,0.99,1.06}};
 	
 	String type;
 	int size;
